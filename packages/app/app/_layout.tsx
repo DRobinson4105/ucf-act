b9565ec2078da5b2bc2fd60dccce8af3b607e641
Raudy Brito <raudyb02@gmail.com>
2025-10-03 19:22:01 -0400

    tab system

--- packages/app/app/_layout.tsx
+++ packages/app/app/_layout.tsx
@@ -1,18 +1,10 @@
 import { Stack } from "expo-router";
-import * as SplashScreen from "expo-splash-screen";
-import { useEffect } from "react";
-import { SafeAreaProvider } from "react-native-safe-area-context";
-
-SplashScreen.preventAutoHideAsync();
+import "../global.css";
 
 export default function RootLayout() {
-  useEffect(() => {
-    SplashScreen.hideAsync();
-  }, []);
-
   return (
-    <SafeAreaProvider>
-      <Stack />
-    </SafeAreaProvider>
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    </Stack>
   );
 }
