f8aa408f5ef21a6ba6c8382a0352d10cdb586d04
Raudy Brito <raudyb02@gmail.com>
2025-10-03 18:57:42 -0400

    development build splash configured

--- packages/app/app/_layout.tsx
+++ packages/app/app/_layout.tsx
@@ -1,5 +1,18 @@
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  return <Stack />;
+  useEffect(() => {
+    SplashScreen.hideAsync();
+  }, []);
+
+  return (
+    <SafeAreaProvider>
+      <Stack />
+    </SafeAreaProvider>
+  );
 }
