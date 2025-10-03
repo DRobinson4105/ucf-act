8975821b41f56042a79f30770b39449f948ad6f4
Raudy Brito <raudyb02@gmail.com>
2025-10-03 19:22:01 -0400

    tab system

---
+++ packages/app/app/(tabs)/_layout.tsx
@@ -0,0 +1,36 @@
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { Tabs } from "expo-router";
+
+export default function TabLayout() {
+  return (
+    <Tabs screenOptions={{ tabBarActiveTintColor: "blue" }}>
+      <Tabs.Screen
+        name="ride"
+        options={{
+          title: "Ride",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="car" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="activity"
+        options={{
+          title: "Activity",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="file" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="user" color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
